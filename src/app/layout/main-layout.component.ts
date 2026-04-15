e84856947b52382255bc301557d78666d8f15213
jose-cassios <cassios.torres.010@gmail.com>
2026-04-15 17:57:31 -0300

    feat(events): implement footer

--- src/app/layout/main-layout.component.ts
+++ src/app/layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { RouterOutlet } from "@angular/router";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-main-layout.component',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
