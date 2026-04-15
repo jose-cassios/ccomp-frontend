85686272d213ba0f270b627431d5871712046bb7
jose-cassios <cassios.torres.010@gmail.com>
2026-04-15 17:00:10 -0300

    feat(events): estruturar events-page e organizar encapsulamento de estilos
    - Criação do componente pai events-page para centralizar seções de eventos.
    - Implementação das seções "Evento em Destaque" e "Próximos Eventos".
    - Padronização de layout com containers e espaçamentos consistentes.
    - Refatoração de CSS para facilitar a futura migração para SCSS.

--- src/app/features/events-page/events-page.component.ts
+++ src/app/features/events-page/events-page.component.ts
@@ -1,35 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
+import { EventoDestaqueComponent } from './components/evento-destaque/evento-destaque.component';
+import { ProximosEventosComponent } from './components/proximos-eventos/proximos-eventos.component'; 
 
 @Component({
   selector: 'app-events-page',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, EventoDestaqueComponent, ProximosEventosComponent],
   templateUrl: './events-page.component.html',
   styleUrl: './events-page.component.css',
 })
 export class EventsPageComponent {
-  // Mock
-  evento = {
-    sigla: 'SEMCOMP',
-    modalidade: 'Presencial',
-    titulo: 'V SEMANA DA COMPUTAÇÃO',
-    descricao:
-      'O maior desafio de programação do estado focado em sustentabilidade algorítmica. Três dias de imersão total para criar soluções que reduzam a pegada de carbono da infraestrutura digital local. Mentoria com especialistas do mercado e premiações exclusivas para os melhores protótipos.',
-    data: '15-17 Out',
-    horario: '08:00 AM',
-    local: 'Auditório',
-    vagas: 120,
-    imagemUrl: 'ifma-logo.png',
-    cargaHoraria: '48 Horas',
-    certificado: true,
-    publico: 'Acadêmicos',
-    preRequisito: 'Lógica de Prog.',
-  };
 
-  inscrever(): void {
-    // No MVP, redireciona para formulário simples
-    console.log('Iniciando jornada de inscrição:');
-  }
 }
