1a03fdd9f7c5b7a3a4587711269c431eefee30ef
JonasNogueiraS <jonasnogueiira@gmail.com>
2026-04-19 20:59:15 -0300

    style(home): padronização do espaçamento entre as seções utilizando flex gap e remoção de margens fixas

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { BannerComponent } from "./components/banner/banner.component";
 import { AgendaMesComponent } from "./components/agenda-mes/agenda-mes.component";
 import { EventoDestaqueComponent } from "./components/evento-destaque/evento-destaque.component";
+import { NoticiasClubeComponent } from "./components/noticias-clube/noticias-clube.component";
+import { DestaquesSemana } from "./components/destaques-semana/destaques-semana";
 
 
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [BannerComponent, AgendaMesComponent, EventoDestaqueComponent],
+  imports: [BannerComponent, AgendaMesComponent, EventoDestaqueComponent, DestaquesSemana, NoticiasClubeComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
